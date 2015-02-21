ab86930494df8a4f828819daf1d53464dc0e1409
Connor Bracewell <connor.bracewell@gmail.com>
2015-02-21 14:12:56 -0800

    changed face

--- src/main/java/ConnorSnake.java
+++ src/main/java/ConnorSnake.java
@@ -1,11 +1,12 @@
 import java.util.HashMap;
 import java.util.Map;
-
+import java.util.ArrayList;
 
 public class ConnorSnake {
 
 	static public String getBoardArray (Map <String, Object> requestBody) {
-		return requestBody.get("board").getClass().toString();
+		ArrayList list = requestBody.get("board");
+		return list.get(0).getClass().toString();
 	}
 
 }
